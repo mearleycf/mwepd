dc1e156432ddb2cbb713e689bb7f822f7d5b7609
mearleycf <mike@earley.info>
2015-05-18 08:34:35 -0400

    monday

---
+++ client/assets/js/app.js
@@ -0,0 +1,39 @@
+(function() {
+  'use strict';
+
+  angular.module('application', [
+    'ui.router',
+    'ngAnimate',
+
+    //foundation
+    'foundation',
+    'foundation.dynamicRouting',
+    'foundation.dynamicRouting.animations'
+  ])
+    .config(config)
+    .run(run)
+  ;
+
+  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+
+  function config($urlProvider, $locationProvider) {
+    $urlProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+      enabled:false,
+      requireBase: false
+    });
+
+    $locationProvider.hashPrefix('!');
+  }
+
+function jsonLoad() {
+  var mydata = JSON.parse(data);
+  alert(mydata[0].specialty);
+}
+
+  function run() {
+    FastClick.attach(document.body);
+  }
+
+})();
